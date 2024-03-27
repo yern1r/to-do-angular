fd884eec657b5535d1f7aa08fc25a3be7fec534c
Yernar <yernaryelemess@gmail.com>
2024-03-27 14:14:14 +0500

    added categories in left panel

--- src/app/views/categories/categories.component.ts
+++ src/app/views/categories/categories.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DataHandlerService } from '../../service/data-handler.service';
+import { Category } from '../../model/Category';
 
 @Component({
   selector: 'app-categories',
@@ -7,12 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriesComponent implements OnInit{
 
-  constructor(){
+ categories!: Category[];
+
+  constructor(
+    private dataHandler: DataHandlerService
+  ){
 
   }
   
+  //method called automatically after init of component
   ngOnInit() {
-    
+    this.categories = this.dataHandler.getCategories();
+    console.log(this.categories);
   }
 
 }
